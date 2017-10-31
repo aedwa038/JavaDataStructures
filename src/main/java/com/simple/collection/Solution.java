0af9038755abf93684b1f06125da146087ded7ad
aedwards72 <aedwards72@gatech.edu>
2017-10-31 19:35:25 -0400

    Adding hackerrank main

---
+++ src/main/java/com/simple/collection/Solution.java
@@ -0,0 +1,21 @@
+package com.simple.collection;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+import java.util.stream.IntStream;
+
+/**
+ * Created by akeem on 10/29/17.
+ */
+public class Solution {
+
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Done!!!");
+    }
+}
